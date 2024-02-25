73d181e2055f01310353d4dee792c3e11e9a9f49
Flippergail <ticarter11@icloud.com>
2024-02-25 14:33:53 +0000

    updates

--- 2022/q1.py
+++ 2022/q1.py
@@ -56,11 +56,8 @@ plt.show()
 
 
 
-# import data from excel spreadsheet
-df = pd.read_excel(r"C:\Users\ticar\Downloads\Remote-work-data.xlsx")
-
 industries = {
-    "Mining, logging, construction": 0,
+    "Mining, logging, construction": [0, 0, 0, 0],
     "Manufacturing": 0,
     "Trade, transportation, and utilities": 0,
     "Information": 0,
@@ -74,26 +71,39 @@ industries = {
 
 numUKEmployeesByIndustryByCity = {
     "Seattle": {
-        "Mining, logging, construction": 0,
-        "Manufacturing": 0,
-        "Trade, transportation, and utilities": 0,
-        "Information": 0,
-        "Financial activities": 0,
-        "Professional and business services": 0,
-        "Education and health services": 0,
-        "Leisure and hospitality": 0,
-        "Other services": 0,
-        "Government": 0,
+        "Mining, logging, construction": [101700	104700	83600	107100	127600	129900	109600],
+        "Manufacturing": [212800	171300	167000	188200	184300	168400	142200],
+        "Trade, transportation, and utilities": [325600	313200	301600	354400	398000	390300	332600],
+        "Information": [79500	77700	87700	97500	128400	133700	139000],
+        "Financial activities": [101800	106700	92100	95900	101400	100400	87600],
+        "Professional and business services": [220500	214400	220700	268600	302100	295700	277500],
+        "Education and health services": [183700	198400	231500	251300	283000	272100	223500],
+        "Leisure and hospitality": [145800	152500	155700	185200	207800	150600	133000],
+        "Other services": [57800	61800	63200	70200	78700	71100	59300],
+        "Government": [236000	252100	264200	270300	275500	266000	206700],
     },
-    'Omaha': industries,
-    'Scranton': industries,
-}
-
-# fills in numUKEmployeesByIndustryByCity
-
-
-
-numUSEmployeesByIndustryByCity = {
-    'Liverpool': industries,
-    'Barry': industries,
+        'Omaha': {
+            "Mining, logging, construction": [23500	25700	20900	25800	30500	30400	30700],
+            "Manufacturing": 0,
+            "Trade, transportation, and utilities": 0,
+            "Information": 0,
+            "Financial activities": 0,
+            "Professional and business services": 0,
+            "Education and health services": 0,
+            "Leisure and hospitality": 0,
+            "Other services": 0,
+            "Government": 0,
+    },
+        'Scranton': {
+            "Mining, logging, construction": [0, 0, 0, 0],
+            "Manufacturing": 0,
+            "Trade, transportation, and utilities": 0,
+            "Information": 0,
+            "Financial activities": 0,
+            "Professional and business services": 0,
+            "Education and health services": 0,
+            "Leisure and hospitality": 0,
+            "Other services": 0,
+            "Government": 0,
+    },
 }
