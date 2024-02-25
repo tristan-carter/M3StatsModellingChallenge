2434b61d82a1d39921532636d94281bc04b00fe8
Flippergail <ticarter11@icloud.com>
2024-02-25 18:13:34 +0000

    Q1 adjustments

--- 2022/q1.py
+++ 2022/q1.py
@@ -8,54 +8,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, r2_score
 
-# data
-years = np.array([2002,2003,2004,2005,2006,2007,2008,2009,2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019])
-percentWorkersHome = np.array([
-    2.90,
-    3.10,
-    3.00,
-    3.10,
-    3.20,
-    3.30,
-    3.50,
-    3.70,
-    3.50,
-    4.00,
-    4.10,
-    4.30,
-    4.50,
-    4.40,
-    4.90,
-    4.80,
-    5.10,
-    5.30,
-])
-
-# plot with linear regression
-plt.scatter(years, percentWorkersHome)
-plt.xlabel('Year')
-plt.ylabel('Percent of Workers Working from Home')
-plt.title('Percent of Workers Working from Home by Year')
-
-# adjusts x ticks
-plt.xticks(np.arange(2002, 2020, 2))
-
-# plots linear regression
-m, b = np.polyfit(years, percentWorkersHome, 1)
-plt.plot(years, m*years + b)
-
-# calculates r^2
-r2 = r2_score(percentWorkersHome, m*years + b)
-print('r^2:', r2)
-
-# extrapolates linear regression to 2027
-#extrapolatedYears = np.array([2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027])
-#extrapolatedPercentWorkersHome = m*extrapolatedYears + b
-#plt.plot(extrapolatedYears, extrapolatedPercentWorkersHome, 'r--')
-
-plt.show()
-
-
 industriesPercentAbleToWorkAtHome = {
     "Mining, logging, construction": 0,
     "Manufacturing": 0,
@@ -67,7 +19,7 @@ industriesPercentAbleToWorkAtHome = {
     "Leisure and hospitality": 0,
     "Other services": 0.5,
     "Government": 0.65,
-},
+}
 
 USIndustryYears = np.array([2000,2005,2010,2015,2019])
 UKIndustryYears = np.array([2005,2010,2015,2019])
@@ -142,18 +94,62 @@ numUKEmployeesByIndustryByCity = {
 for city in numUKEmployeesByIndustryByCity:
     for industry in numUKEmployeesByIndustryByCity[city]:
         numUKEmployeesByIndustryByCity[city][industry] = np.array(numUKEmployeesByIndustryByCity[city][industry])
-        # removes final two years from data
+        # removes final two years from data due to covid skewing data
         numUKEmployeesByIndustryByCity[city][industry] = numUKEmployeesByIndustryByCity[city][industry][:-2]
 
 for city in numUSEmployeesByIndustryByCity:
     for industry in numUSEmployeesByIndustryByCity[city]:
         numUSEmployeesByIndustryByCity[city][industry] = np.array(numUSEmployeesByIndustryByCity[city][industry])
-        # removes final two years from data
+        # removes final two years from data due to covid skewing data
         numUSEmployeesByIndustryByCity[city][industry] = numUSEmployeesByIndustryByCity[city][industry][:-2]
 
+# data
+years = np.array([2002,2003,2004,2005,2006,2007,2008,2009,2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019])
+percentWorkersHome = np.array([
+    2.90,
+    3.10,
+    3.00,
+    3.10,
+    3.20,
+    3.30,
+    3.50,
+    3.70,
+    3.50,
+    4.00,
+    4.10,
+    4.30,
+    4.50,
+    4.40,
+    4.90,
+    4.80,
+    5.10,
+    5.30,
+])
 
+# plot with linear regression
+plt.scatter(years, percentWorkersHome)
+plt.xlabel('Year')
+plt.ylabel('Percent of Workers Working from Home')
+plt.title('Percent of Workers Working from Home by Year')
+
+# adjusts x ticks
+plt.xticks(np.arange(2002, 2020, 2))
+
+# plots linear regression
+m, b = np.polyfit(years, percentWorkersHome, 1)
+plt.plot(years, m*years + b)
+
+# calculates r^2
+r2 = r2_score(percentWorkersHome, m*years + b)
+print('r^2:', r2)
+
+plt.show()
 
 # plot with linear regression for each industry for each city in US and record values at 2024 and 2027
+minimumR2 = 1
+totalR2 = 0
+numR2Values = 0
+
 i = 0
 for country in [numUSEmployeesByIndustryByCity, numUKEmployeesByIndustryByCity]:
     i += 1
@@ -167,6 +163,9 @@ for country in [numUSEmployeesByIndustryByCity, numUKEmployeesByIndustryByCity]:
         legendList = []
         legendLinesList = []
         for industry in country[city]:
+            # plots points
+            plt.scatter(industryYears, country[city][industry])
+
             # adjusts x ticks and rotates them 45 degrees
             plt.xticks(np.arange(2000, 2027)[::2], rotation=40)
 
@@ -176,9 +175,13 @@ for country in [numUSEmployeesByIndustryByCity, numUKEmployeesByIndustryByCity]:
 
             # calculates r^2
             r2 = r2_score(country[city][industry], m*industryYears + b)
+            totalR2 += r2
+            numR2Values += 1
+            if r2 < minimumR2:
+                minimumR2 = r2
 
             # adds to legend
-            legendList.append(f'Linear Regression of {industry} number of workers, r^2: {round(r2, 2)}')
+            legendList.append(f'Linear Regression of {industry} number of workers, r^2: {round(r2, 3)}')
             legendLinesList.append(Line2D([0], [0], color=linearRegression.get_color(), lw=2))
 
             # extrapolates linear regression to 2027
@@ -192,13 +195,17 @@ for country in [numUSEmployeesByIndustryByCity, numUKEmployeesByIndustryByCity]:
             country[city][industry] = np.append(country[city][industry], valueAt2024)
             country[city][industry] = np.append(country[city][industry], valueAt2027)
 
-        plt.legend(legendList, bbox_to_anchor=(1.05, 0.8), loc='upper left')
+        plt.legend(legendLinesList, legendList, bbox_to_anchor=(1.05, 0.8), loc='upper left')
         plt.show()
+
+print('Minimum r^2:', round(minimumR2, 5))
+print('Mean r^2:', round(totalR2 / numR2Values, 5))
 
 for country in [numUSEmployeesByIndustryByCity, numUKEmployeesByIndustryByCity]:
     for city in country:
         totalWorkers2024 = 0
         totalWorkers2027 = 0
+        
         totalWorkersWhoCanWorkFromHome2024 = 0
         totalWorkersWhoCanWorkFromHome2027 = 0
         for industry in country[city]:
